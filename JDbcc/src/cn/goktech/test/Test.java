90f4d13020aa03ecbf45eb443eba0c09a8517408
pengaoqq <2496310538@qq.com>
2019-01-06 11:07:02 +0800

    是是是

--- JDbcc/src/cn/goktech/test/Test.java
+++ JDbcc/src/cn/goktech/test/Test.java
@@ -1,7 +1,7 @@
 package cn.goktech.test;
 
 public class Test {
-	//abc1231xd11
+	//abc1231xd1111111
 	public static void main(String[] args) {
 		int[] a ={5,1,2,3,8,1,4};
 		for (int i = 0; i < a.length; i++) {
