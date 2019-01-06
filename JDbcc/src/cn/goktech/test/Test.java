c99a946d6229a8a3c9d06eb491d49c5dd8a30b14
pengaoqq <46395156+pengaoqq@users.noreply.github.com>
2019-01-06 11:10:30 +0800

    Update Test.java

--- JDbcc/src/cn/goktech/test/Test.java
+++ JDbcc/src/cn/goktech/test/Test.java
@@ -3,6 +3,7 @@ package cn.goktech.test;
 public class Test {
 	//abc1231xd1111111
 	public static void main(String[] args) {
+		//测试
 		int[] a ={5,1,2,3,8,1,4};
 		for (int i = 0; i < a.length; i++) {
 //			for (int k = 0; k < a.length; k++) {
